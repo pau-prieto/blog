8719f5b72d6e576f475a1d77ce3c970d85e07319
Paula Prieto <pprieto@myune.edu.au>
2024-07-20 14:07:29 +1000

    Created blade view for create

---
+++ resources/views/Posts/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+   <h1>Blog Post Create</h1>
+   <form action="{{ route('posts.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="content">Content</label>
+            <textarea name="content" class="form-control" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+@endsection
