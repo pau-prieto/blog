9bc968f9b2fca293c190d1bf9be435de6cfa24a2
Paula Prieto <pprieto@myune.edu.au>
2024-09-11 16:40:51 +1000

    Updated "BLOG" link URL in other blades

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,6 +49,12 @@
                                             Register
                                         </a>
                                     @endif
+                                    <a
+                                        href="http://localhost:3000"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Blog
+                                    </a>
                                 @endauth
                             </nav>
                         @endif
